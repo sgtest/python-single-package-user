2665fd861b3ed51d187413596628371fc0f9d8f3
Beyang Liu <beyang@sourcegraph.com>
2018-03-03 15:36:50 -0800

    add use

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import mypkg.m
+from mypkg import m
 from werkzeug.utils import import_string
 
 def run():
     mypkg.m.helloWorld()
+    m.helloWorld()
 
 def useWerkzeug():
     import_string('', silent=True)
